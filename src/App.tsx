aaaf9f17ebd76ac5f161b8ac008330556bdc7873
amp-r3 <amp_r3@proton.me>
2025-11-19 06:06:58 +0500

    feat: added smooth scroll by lenis lib

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import Header from "./components/Header/Header"
 import HeroSection from "./components/HeroSection/HeroSection"
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
 import SkillsSection from "./components/SkillsSection/SkillsSection"
+import { SmoothScroll } from "./components/SmoothScroll/SmoothScroll"
 
 const App = () => {
   return (
-    <>
+    <SmoothScroll>
       <Header />
       <HeroSection />
       <SkillsSection />
       <ProjectsSection />
       <ContactSection />
       <Footer />
-    </>
+    </SmoothScroll>
   )
 }
 
