3566a4ba6eab75309091c745599005381366d856
amp-r3 <amp_r3@proton.me>
2025-12-26 18:13:34 +0500

    feat: add 404 page for invalid routes

--- src/Pages/Project/ProjectPage.tsx
+++ src/Pages/Project/ProjectPage.tsx
@@ -1,3 +1,4 @@
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary"
 import ProjectDetails from "@/components/ProjectDetails/ProjectDetails"
 import { projectsData } from "@/data/projectsData"
 import { useParams } from "react-router"
@@ -6,7 +7,7 @@ const ProjectPage = () => {
   const { id } = useParams()
 
   const project = projectsData.find(p => p.id === id)
-  if (!project) return <div>Project not found</div>
+  if (!project) return <ErrorBoundary />
   return (
     <>
       <ProjectDetails
--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,24 +1,34 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router"; 
 import { Paths } from "./paths";
 import App from "@/App";
 import Home from "@/Pages/Home/Home";
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
 
 export const router = createBrowserRouter([
     {
-        path: Paths.home,
-        Component: App,
+        path: Paths.home, 
+        Component: App, 
         children: [
             {
-                index: true,
-                Component: Home,
-            },
-            {
-                path: Paths.projectPage,
-                lazy: async () => {
-                    const module = await import ("@/Pages/Project/ProjectPage");
-                    return { Component: module.default }
-                }
+                ErrorBoundary: ErrorBoundary,
+                children: [
+                    {
+                        index: true,
+                        Component: Home,
+                    },
+                    {
+                        path: Paths.projectPage,
+                        lazy: async () => {
+                            const module = await import("@/Pages/Project/ProjectPage");
+                            return { Component: module.default }
+                        },
+                    },
+                    {
+                        path: "*",
+                        Component: ErrorBoundary,
+                    }
+                ]
             }
         ]
     }
-])
+]);
